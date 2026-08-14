e22cbc2527d826786aa5788ff1b98c79dd09fbfe
tx1f6 <juan.fernandez@six-group.com>
2026-08-14 16:25:25 +0200

    sql

---
+++ source/src/main/java/com/sixgroup/refit/observability/config/DatasourceSchemaProperties.java
@@ -0,0 +1,20 @@
+package com.sixgroup.refit.observability.config;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "component-config.kududb")
+public class DatasourceSchemaProperties {
+    @NotNull
+    private String controlRefitSchema;
+
+    @NotNull
+    private String transcSchema;
+
+    @NotNull
+    private String accountMng;
+}
---
+++ source/src/main/java/com/sixgroup/refit/observability/config/Item32Properties.java
@@ -0,0 +1,52 @@
+package com.sixgroup.refit.observability.config;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("component-config.item32")
+public class Item32Properties {
+    @NotNull
+    private String trCode;
+    @NotNull
+    private String regulationReference;
+    @NotNull
+    private Item32AProperties item32Aproperties;
+    @NotNull
+    private Item32BProperties item32BProperties;
+    @NotNull
+    private Item32C item32C;
+
+    @Getter
+    @Setter
+    public static class Item32AProperties {
+
+        @NotNull
+        private Long initialTotalTradesNew;
+        @NotNull
+        private Long initialTotalTradesAll;
+        @NotNull
+        private String fileNamePattern;
+    }
+
+    @Getter
+    @Setter
+    public static class Item32BProperties {
+        @NotNull
+        private String fileNamePattern;
+    }
+
+    @Getter
+    @Setter
+    public static class Item32C {
+        @NotNull
+        private String fileNamePattern;
+
+        private String customerAccountPattern;
+
+        private String regulatorAccountPattern;
+    }
+}
