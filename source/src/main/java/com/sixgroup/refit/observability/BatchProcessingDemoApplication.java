bf17a14cbc8ed994767c1290b9daa1f959cac36f
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-08-20 17:34:18 +0200

    Register configuration properties for local startup

--- source/src/main/java/com/sixgroup/refit/observability/BatchProcessingDemoApplication.java
+++ source/src/main/java/com/sixgroup/refit/observability/BatchProcessingDemoApplication.java
@@ -1,10 +1,11 @@
 package com.sixgroup.refit.observability;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BatchProcessingDemoApplication {
 
     public static void main(String[] args) {
