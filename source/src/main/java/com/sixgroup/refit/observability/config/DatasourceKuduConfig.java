21b4a6f1f8d3a2d08cdd9f455356d3ff382692c3
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-08-21 16:51:38 +0200

    fix: remove unused JPA dependency from item32c datasource

--- source/src/main/java/com/sixgroup/refit/observability/config/DatasourceKuduConfig.java
+++ source/src/main/java/com/sixgroup/refit/observability/config/DatasourceKuduConfig.java
@@ -1,31 +1,19 @@
 package com.sixgroup.refit.observability.config;
 
-import java.util.HashMap;
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @ConditionalOnProperty(prefix = "spring.datasource.kududb", name = "jdbc-url")
-@EnableJpaRepositories(
-        entityManagerFactoryRef = "kudurc-em", transactionManagerRef = "kudurc-trm", basePackages = "com.sixgroup.refit.observability.item37.creator.modules.infrastructure.repository.kudu"
-)
-@EnableTransactionManagement
 @RequiredArgsConstructor
 public class DatasourceKuduConfig {
 
@@ -41,25 +29,8 @@ public class DatasourceKuduConfig {
                 .build();
     }
 
-    @Bean("kudurc-em")
-    LocalContainerEntityManagerFactoryBean kuduDbEntityManagerFactory(EntityManagerFactoryBuilder builder, @Qualifier("kudurc-ds") DataSource dataSource) {
-        final HashMap<String, String> objectObjectHashMap = new HashMap<>();
-        objectObjectHashMap.put(DatasourceKuduProperties.HIBERNATE_DIALECT, properties.getDialect());
-        objectObjectHashMap.put(DatasourceKuduProperties.HIBERNATE_DDL_AUTO, properties.getDdlAuto());
-        return builder
-                .dataSource(dataSource)
-                .persistenceUnit(properties.getPersistenceUnit())
-                .properties(objectObjectHashMap)
-                .build();
-    }
-
     @Bean("kudurc-trm")
-    PlatformTransactionManager kuduTransactionManager(@Qualifier("kudurc-em") EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    @Bean
-    public EntityManagerFactoryBuilder entityManagerFactoryBuilder() {
-        return new EntityManagerFactoryBuilder(new HibernateJpaVendorAdapter(), new HashMap<>(), null);
+    PlatformTransactionManager kuduTransactionManager(@Qualifier("kudurc-ds") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
