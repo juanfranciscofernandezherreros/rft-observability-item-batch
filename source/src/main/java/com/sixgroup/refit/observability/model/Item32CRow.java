d1ba4569464baddc12666937afc9976e4fde7d9c
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-08-21 11:42:24 +0200

    feat(item32c): add CSV row model

---
+++ source/src/main/java/com/sixgroup/refit/observability/model/Item32CRow.java
@@ -0,0 +1,19 @@
+package com.sixgroup.refit.observability.model;
+
+public record Item32CRow(
+        String regulatorId,
+        String reportCode,
+        String queryId,
+        String channel,
+        String reportType,
+        String deliveryDateFrom,
+        String deliveryDateTo,
+        String insertTimestamp,
+        String reportSchedule,
+        String reportFrequencyDaily,
+        String reportFrequencyMonth,
+        String lastDayOfMonth,
+        String reportFormat,
+        String reportStatus,
+        String partition) {
+}
